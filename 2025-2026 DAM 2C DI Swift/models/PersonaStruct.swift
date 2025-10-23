d82e2fe3b10a900886375ef4044c4ff26b6e79a6
RicardoDigitech <ricardo.sornosa@digitechfp.com>
2025-10-23 20:54:20 +0200

    Finalización.

---
+++ 2025-2026 DAM 2C DI Swift/models/PersonaStruct.swift
@@ -0,0 +1,8 @@
+//
+//  PersonaStruct.swift
+//  2025-2026 DAM 2C DI Swift
+//
+//  Created by Ricardo on 23/10/25.
+//
+
+import Foundation
---
+++ 2025-2026 DAM 2C DI Swift/models/RelojPulseraModel.swift
@@ -0,0 +1,8 @@
+//
+//  RelojPulseraModel.swift
+//  2025-2026 DAM 2C DI Swift
+//
+//  Created by Ricardo on 22/10/25.
+//
+
+import Foundation
